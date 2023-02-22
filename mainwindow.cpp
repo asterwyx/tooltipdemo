ebb53982a4f437047ebf4d5248f24898246fb042
Astrea <astreaonearth@gmail.com>
2023-02-22 23:20:16 +0800

    feat: finish demo
    Finish demo and simplify code.
    
    Log: finish demo with simplified code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,13 @@
-#include "mainwindow.hpp"
+#include "mainwindow.h"
 #include <QDebug>
 #include <DToolTip>
-#include <QAbstractListModel>
-#include "selectormodel.hpp"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       m_centralWidget(new QWidget(this)),
       m_listViewTestGroup(new QGroupBox(this)),
       m_labelTestGroup(new QGroupBox(this)),
       m_listView(new DListView(m_listViewTestGroup)),
-      m_label(new DLabel(m_labelTestGroup)),
       m_contentLayout(new QHBoxLayout),
       m_listViewLayout(new QVBoxLayout),
       m_labelLayout(new QVBoxLayout),
@@ -23,7 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
     m_contentLayout->addWidget(m_listViewTestGroup, 1);
     m_contentLayout->addWidget(m_labelTestGroup, 1);
     m_centralWidget->setLayout(m_contentLayout);
-
+    static constexpr int InitialIndex = 2;
+    static constexpr DToolTip::ToolTipShowMode InitialMode = DToolTip::ShowWhenElided;
     // Setup ListView test
     m_listViewTestGroup->setLayout(m_listViewLayout);
     // Select DListView ToolTip show mode
@@ -31,54 +30,84 @@ MainWindow::MainWindow(QWidget *parent)
         QHBoxLayout *selectorLayout = new QHBoxLayout;
         m_listViewHint->setText("选择DListView的ToolTip显示方式:");
         selectorLayout->addWidget(m_listViewHint);
-        m_listViewSelector->setModel(new SelectorModel);
+        m_listViewSelector->addItem("默认", DToolTip::Default);
+        m_listViewSelector->addItem("始终显示", DToolTip::AlwaysShow);
+        m_listViewSelector->addItem("折叠时显示", DToolTip::ShowWhenElided);
+        m_listViewSelector->addItem("始终不显示", DToolTip::NotShow);
+        m_listViewSelector->setCurrentIndex(InitialIndex); // ShowWhenElided
         selectorLayout->addWidget(m_listViewSelector);
         m_listViewLayout->addLayout(selectorLayout);
     }
+    // Connect signal
+    connect(m_listViewSelector, SIGNAL(currentIndexChanged(int)), this, SLOT(onIndexChanged(int)));
     // Setup ListView
     m_listViewLayout->addWidget(m_listView);
-    initializeModel();
+    auto addPlainItem = [&](const QString &item) {
+        DStandardItem *pItem = new DStandardItem(item);
+        pItem->setEditable(false);
+        m_model->appendRow(pItem);
+    };
+    addPlainItem("短项");
+    addPlainItem("长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长项");
+    addPlainItem("This is a very long item composed by a lot of short English words to test wrap");
+    addPlainItem("Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong word item");
+    addPlainItem("带有换行符号的项\n第二句只是填充");
     m_listView->setItemSpacing(2);
+    m_listView->setFixedWidth(WidgetWidth);
     m_listView->setModel(m_model);
     m_listView->setUniformItemSizes(true);
     m_listView->setWordWrap(false);
     m_listView->setTextElideMode(Qt::ElideRight);
-    DToolTip::setToolTipShowMode(m_listView, DToolTip::ShowWhenElided);
-
+    DToolTip::setToolTipShowMode(m_listView, InitialMode);
     // Setup DLabel test
     m_labelTestGroup->setLayout(m_labelLayout);
-
     // Select DLabel ToolTip show mode
     {
         QHBoxLayout *selectorLayout = new QHBoxLayout;
         m_labelHint->setText("选择DLabel的ToolTip显示方式:");
         selectorLayout->addWidget(m_labelHint);
-        QListView *view = new QListView;
-        SelectorModel *selector = new SelectorModel;
-        view->setModel(selector);
-        m_labelSelector->setModel(selector);
-        m_labelSelector->setView(view);
+        m_labelSelector->addItem("默认", DToolTip::Default);
+        m_labelSelector->addItem("始终显示", DToolTip::AlwaysShow);
+        m_labelSelector->addItem("折叠时显示", DToolTip::ShowWhenElided);
+        m_labelSelector->addItem("始终不显示", DToolTip::NotShow);
+        m_labelSelector->setCurrentIndex(InitialIndex); // ShowWhenElided
         selectorLayout->addWidget(m_labelSelector);
         m_labelLayout->addLayout(selectorLayout);
     }
-    m_labelLayout->addWidget(m_label);
-    m_label->setText("这是一个非常长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长的测试Label");
-    m_label->setElideMode(Qt::ElideRight);
-    DToolTip::setToolTipShowMode(m_label, DToolTip::ShowWhenElided);
+    // Connect signal
+    connect(m_labelSelector, SIGNAL(currentIndexChanged(int)), this, SLOT(onIndexChanged(int)));
+    auto setupLabel = [&] (const QString &text) {
+        DLabel *testLabel = new DLabel(this);
+        testLabel->setText(text);
+        testLabel->setFixedWidth(WidgetWidth);
+        testLabel->setElideMode(Qt::ElideRight);
+        m_labelLayout->addWidget(testLabel);
+        DToolTip::setToolTipShowMode(testLabel, InitialMode);
+        m_labels.append(testLabel);
+    };
+
+    setupLabel("这是一个非常长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长的测试Label");
+    setupLabel("短Label");
+    setupLabel("带有换行符号的测试Label\n第二个句子仅仅是填充");
+    setupLabel("This is a very long label composed by a lot of short English words to test wrap");
+    setupLabel("Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong word label");
 }
 
-void MainWindow::addPlainItem(const QString &name)
+void MainWindow::onIndexChanged(int index)
 {
-    DStandardItem *pItem = new DStandardItem(name);
-    pItem->setEditable(false);
-    m_model->appendRow(pItem);
-}
-
-void MainWindow::initializeModel()
-{
-    addPlainItem("This is first item.");
-    addPlainItem("Short.");
-    addPlainItem("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong item within one word");
-    addPlainItem("This is a very long item, but it is composed by a lot of short words.");
-    addPlainItem("With newline.\nThe second sentence is just dummy.");
+    QComboBox *selector = qobject_cast<QComboBox *>(sender());
+    if (selector)
+    {
+        DToolTip::ToolTipShowMode mode = qvariant_cast<DToolTip::ToolTipShowMode>(selector->itemData(index));
+        if (selector == m_listViewSelector) {
+            DToolTip::setToolTipShowMode(m_listView, mode);
+        } else if (selector == m_labelSelector) {
+            for(auto &label : m_labels)
+            {
+                DToolTip::setToolTipShowMode(label, mode);
+            }
+        } else {
+            Q_UNUSED(index)
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -10,19 +10,20 @@
 DWIDGET_USE_NAMESPACE
 
 class MainWindow : public QMainWindow {
+    Q_OBJECT
 
 public:
+    static constexpr quint32 WidgetWidth = 400;
     MainWindow(QWidget *parent = nullptr);
 
-    void initializeModel();
-    void addPlainItem(const QString &name);
+public Q_SLOTS:
+    void onIndexChanged(int index);
 
 private:
     QWidget *m_centralWidget;
     QGroupBox *m_listViewTestGroup;
     QGroupBox *m_labelTestGroup;
     DListView *m_listView;
-    DLabel *m_label;
     QHBoxLayout *m_contentLayout;
     QVBoxLayout *m_listViewLayout;
     QVBoxLayout *m_labelLayout;
@@ -31,4 +32,5 @@ private:
     QComboBox *m_listViewSelector;
     QLabel *m_labelHint;
     QComboBox *m_labelSelector;
+    QList<DLabel *> m_labels;
 };
